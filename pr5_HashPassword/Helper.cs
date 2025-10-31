1b1e6e624bf018a14568338d8b9f7c0354cf0aaf
1qwe-qwe1 <qudasetl@gmail.com>
2025-10-31 17:28:28 +0700

    Добавьте файлы проекта.

---
+++ pr5_HashPassword/Helper.cs
@@ -0,0 +1,39 @@
+﻿using pr5_HashPassword.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HashPasswords;
+
+namespace pr5_HashPassword
+{
+    public class Helper
+    {
+        private Entities _context; 
+
+        public Helper()
+        {
+            _context = new Entities();
+        }
+
+        
+        public static Entities GetContext()
+        {
+            return new Entities();
+        }
+
+        public void CreateUser(Users user, string password)
+        {
+            user.PasswordHash = Hash.HashPassword(password);
+            user.RegistrationDate = DateTime.Now;
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        
+
+
+    }
+}
---
+++ pr5_HashPassword/Program.cs
@@ -0,0 +1,51 @@
+﻿using HashPasswords;
+using pr5_HashPassword.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr5_HashPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Система регистрации пользователей");
+
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Введите пароль: ");
+            string password = Console.ReadLine();
+
+            string hashedPassword = Hash.HashPassword(password);
+
+            Users newUser = new Users
+            {
+                Login = login,
+                PasswordHash = hashedPassword,
+                RegistrationDate = DateTime.Now
+            };
+
+            try
+            {
+                Helper helper = new Helper();
+                helper.CreateUser(newUser, password);
+
+                Console.WriteLine($"Пользователь {newUser.Login} создан!");
+                Console.WriteLine($"Хеш пароля: {newUser.PasswordHash}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при сохранении в БД: {ex.Message}");
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine($"Пользователь {newUser.Login} создан!");
+            Console.WriteLine($"Хеш пароля: {newUser.PasswordHash}");
+        }
+    }
+}
